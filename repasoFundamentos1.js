1fb23d231a59dbecd7b1acee25611e611579366e
Enrique Castrillo <81467287+Kikelon@users.noreply.github.com>
2021-10-20 20:47:45 +0200

    Subiendo el resto del reto

--- repasoFundamentos1.js
+++ repasoFundamentos1.js
@@ -78,18 +78,85 @@ function zodiaco(fecha) {
     }
     return signo;
 }
-var fecha = new Date(2000, 0, 20);
-console.log(fecha);
-console.log(zodiaco(fecha));
-// Capricornio: 22-12 al 19-1.
-// Acuario: 20-1 al 18-2.
-// Piscis: 19-2 al 20-3
-// Aries: 21-3 al 19-4.
-// Tauro: 20-4 al 20-5.
-// Géminis: 21-5 al 20-6.
-// Cáncer: 21-6 al 22-7.
-// Leo: 23-7 al 22-8.
-// Virgo: 23-8 al 22-9.
-// Libra: 23-9 al 22-10.
-// Escorpio: 23-10 al 21-11.
-// Sagitario: 22-11 al 21-12.
+// let fecha = new Date(2000, 0, 20);
+// console.log(fecha);
+// console.log(zodiaco(fecha))
+function continenteDelPais(dondeEstoy) {
+    var continente = ["Asia", "América", "Árica", "Europa", "Oceanía"];
+    var mundo = [
+        // Asia
+        ["Afganistan", "Arabia Saudita", "Armenia", "Azerbaiyán", "Bangladés"],
+        // América
+        ["Antigua y Barbuda", "Argentina", "Bahamas", "Barbados", "Belice"],
+        // Árica
+        ["Angola", "Argelia", "Benín", "Botsuana", "Burkina Faso"],
+        // Europa
+        ["Albania", "Alemania", "Andorra", "Armenia", "Austria"],
+        // Oceanía
+        ["Australia", "Nueva Zelanda", "Papúa Nueva Guinea", "Fiyi", "Islas Marshall"]
+    ];
+    for (var cont = 0; cont < 5; cont++) {
+        for (var pais = 0; pais < 5; pais++) {
+            (dondeEstoy == mundo[cont][pais]) ? (console.log(continente[cont]), pais = 5, cont = 5) : false;
+        }
+    }
+}
+// console.log(continenteDelPais("Botsuana"));
+function imparesHasta(n) {
+    for (var i = 1; i <= n; i = i + 2)
+        console.log(i);
+}
+// imparesHasta(10);
+function invertirArray(array) {
+    var resultado = [];
+    array.forEach(function (element) { resultado.unshift(element); });
+    return resultado;
+}
+// let arrayNumeros : number[] = [1, 2, 3, 4, 5];
+// let arrayStrings : string[] = ["primero", "segundo", "tercero", "cuarto", "quinto"]
+// console.log(invertirArray(arrayNumeros));
+// console.log(invertirArray(arrayStrings));
+function arcoiris(colores) {
+    var coloresArcoiris = ["rojo", "naranja", "amarillo", "verde", "azul", "arcoíris ", "violeta"];
+    colores.forEach(function (element) { (coloresArcoiris.indexOf(element.toLowerCase()) > (-1)) ? console.log(element + " está en el arcoíris") : console.log(element + " no está en el arcoíris"); });
+}
+// let colores : string[] = ["Rojo", "magenta", "cian", "negro", "amaRillo", "verde"]
+// arcoiris(colores)
+function existePar(numeros) {
+    var resultado = false;
+    numeros.forEach(function (element) { ((element % 2) == 0) ? resultado = true : 1; });
+    return resultado;
+}
+// let arrayNumeros : number[] = [3, 5, 1, 6, 13, 7, 3];
+// console.log(existePar(arrayNumeros));
+function todosEmpiezanPorM(nombres) {
+    var resultado = true;
+    nombres.forEach(function (element) { (element.charAt(0) == "M") ? true : resultado = false; });
+    return resultado;
+}
+// let arrayEmes : string[] = ["Marcos", "Madera", "Mostaza", "Maestro"]
+// console.log(todosEmpiezanPorM(arrayEmes));
+// let arrayVariado : string[] = ["Rojo", "Madera", "tercero", "Cáncer"]
+// console.log(todosEmpiezanPorM(arrayVariado));
+function sumaCaracteres(array) {
+    var resultado = 0;
+    array.forEach(function (element) { resultado += element.length; });
+    return resultado;
+}
+// let arrayLetras : string[] = ["Rojo", "magenta", "cian", "negro", "amaRillo", "verde"]
+// console.log(sumaCaracteres(arrayLetras));
+function parImpar(numero) {
+    (numero % 2 != 0) ? console.log("El número es impar") : console.log("El número es par");
+}
+// let numero : number = Math.round(Math.random() * 10);
+// console.log("Este es el número: ", numero)
+// parImpar(numero)
+var array1 = ["Casa", "Coche", "Ciudad", "Cesta"];
+var array2 = ["Barco", "Baca", "Bicicleta", "Balon", "Bisiesto", "Brasil"];
+var array3 = ["Venezuela", "Veneno", "Voltaje"];
+// console.log("La suma de los caracteres del array 1 es: ", sumaCaracteres(array1));
+// parImpar(sumaCaracteres(array1));
+// console.log("La suma de los caracteres del array 2 es: ", sumaCaracteres(array2));
+// parImpar(sumaCaracteres(array2));
+// console.log("La suma de los caracteres del array 3 es: ", sumaCaracteres(array3));
+// parImpar(sumaCaracteres(array3));
--- repasoFundamentos1.ts
+++ repasoFundamentos1.ts
@@ -56,21 +56,107 @@ function zodiaco(fecha: Date): string{
     return signo;
 }
 
-let fecha = new Date(2000, 0, 20);
-console.log(fecha);
-console.log(zodiaco(fecha))
+// let fecha = new Date(2000, 0, 20);
+// console.log(fecha);
+// console.log(zodiaco(fecha))
+
+function continenteDelPais(dondeEstoy: string){
+    let continente : string[] = ["Asia", "América", "Árica", "Europa", "Oceanía"];
+    let mundo : string [][] = [
+    // Asia
+        ["Afganistan", "Arabia Saudita", "Armenia", "Azerbaiyán", "Bangladés"],
+    // América
+        ["Antigua y Barbuda", "Argentina", "Bahamas", "Barbados", "Belice"],
+    // Árica
+        ["Angola", "Argelia", "Benín", "Botsuana", "Burkina Faso"],
+    // Europa
+        ["Albania", "Alemania", "Andorra", "Armenia", "Austria"],
+    // Oceanía
+        ["Australia", "Nueva Zelanda", "Papúa Nueva Guinea", "Fiyi", "Islas Marshall"]
+    ]
+    for (let cont = 0; cont < 5; cont++){
+        for (let pais = 0; pais < 5; pais++){
+            (dondeEstoy == mundo[cont][pais]) ? (console.log(continente[cont]), pais = 5, cont = 5) :  false;
+        }
+    }
+}
+
+// console.log(continenteDelPais("Botsuana"));
+
+function imparesHasta (n : number){
+    for (let i = 1; i <= n; i = i + 2) console.log(i)
+}
+
+// imparesHasta(10);
 
 
-// Capricornio: 22-12 al 19-1.
-// Acuario: 20-1 al 18-2.
-// Piscis: 19-2 al 20-3
-// Aries: 21-3 al 19-4.
-// Tauro: 20-4 al 20-5.
-// Géminis: 21-5 al 20-6.
-// Cáncer: 21-6 al 22-7.
-// Leo: 23-7 al 22-8.
-// Virgo: 23-8 al 22-9.
-// Libra: 23-9 al 22-10.
-// Escorpio: 23-10 al 21-11.
-// Sagitario: 22-11 al 21-12.
+function invertirArray (array: any[]): any[]{
+    let resultado : any[] = [];
+    array.forEach(element => {resultado.unshift(element)});
+    return resultado;
+}
+
+// let arrayNumeros : number[] = [1, 2, 3, 4, 5];
+// let arrayStrings : string[] = ["primero", "segundo", "tercero", "cuarto", "quinto"]
+// console.log(invertirArray(arrayNumeros));
+// console.log(invertirArray(arrayStrings));
+
+function arcoiris (colores: string[]){
+    let coloresArcoiris : string[] = ["rojo", "naranja", "amarillo", "verde", "azul", "arcoíris ", "violeta"];
+    colores.forEach(element => {(coloresArcoiris.indexOf(element.toLowerCase())>(-1)) ? console.log(element + " está en el arcoíris") : console.log(element + " no está en el arcoíris")});
+}
+
+// let colores : string[] = ["Rojo", "magenta", "cian", "negro", "amaRillo", "verde"]
+// arcoiris(colores)
+
+function existePar (numeros: number[]): boolean{
+    let resultado : boolean = false;
+    numeros.forEach(element => { ((element % 2) == 0) ? resultado = true : 1});
+    return resultado;
+}
+
+// let arrayNumeros : number[] = [3, 5, 1, 6, 13, 7, 3];
+// console.log(existePar(arrayNumeros));
+
+function todosEmpiezanPorM (nombres: string[]): boolean{
+    let resultado : boolean = true;
+    nombres.forEach(element => {(element.charAt(0) == "M") ? true : resultado = false});
+    return resultado;
+}
+
+// let arrayEmes : string[] = ["Marcos", "Madera", "Mostaza", "Maestro"]
+// console.log(todosEmpiezanPorM(arrayEmes));
+// let arrayVariado : string[] = ["Rojo", "Madera", "tercero", "Cáncer"]
+// console.log(todosEmpiezanPorM(arrayVariado));
+
+
+function sumaCaracteres(array : string[]): number{
+    let resultado : number = 0;
+    array.forEach(element => {resultado += element.length})
+    return resultado;
+}
+
+// let arrayLetras : string[] = ["Rojo", "magenta", "cian", "negro", "amaRillo", "verde"]
+// console.log(sumaCaracteres(arrayLetras));
+
+function parImpar(numero : number){
+    (numero % 2 != 0) ? console.log("El número es impar") : console.log("El número es par");
+}
+
+// let numero : number = Math.round(Math.random() * 10);
+// console.log("Este es el número: ", numero)
+// parImpar(numero)
+
+let array1 : string[] =["Casa", "Coche", "Ciudad", "Cesta"];
+let array2 : string[] = ["Barco", "Baca", "Bicicleta", "Balon", "Bisiesto", "Brasil"];
+let array3 : string[] =["Venezuela", "Veneno", "Voltaje"];
+
+// console.log("La suma de los caracteres del array 1 es: ", sumaCaracteres(array1));
+// parImpar(sumaCaracteres(array1));
+// console.log("La suma de los caracteres del array 2 es: ", sumaCaracteres(array2));
+// parImpar(sumaCaracteres(array2));
+// console.log("La suma de los caracteres del array 3 es: ", sumaCaracteres(array3));
+// parImpar(sumaCaracteres(array3));
+
+
 
